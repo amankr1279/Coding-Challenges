20022c1728413e0789adaca1defdd22a93275779
Aman Kumar <aman.q.kumar@oracle.com>
2024-12-12 00:46:02 +0530

    Refactor

--- ccurl_shortner/urlshortner/src/main/java/com/cc/urlshortner/UrlShortenerController.java
+++ ccurl_shortner/urlshortner/src/main/java/com/cc/urlshortner/UrlShortenerController.java
@@ -4,14 +4,11 @@ import com.cc.urlshortner.model.AddUrlRequest;
 import com.cc.urlshortner.model.AddUrlResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.net.http.HttpResponse;
 
 @RestController
 public class UrlShortenerController {
