c81b334461a993445fea6460b572f229767388ee
Aman Kumar <aman.q.kumar@oracle.com>
2024-12-12 12:51:57 +0530

    Add findByShortUrl method

--- ccurl_shortner/urlshortner/src/main/java/com/cc/urlshortner/repo/UrlDataRepo.java
+++ ccurl_shortner/urlshortner/src/main/java/com/cc/urlshortner/repo/UrlDataRepo.java
@@ -4,7 +4,9 @@ import com.cc.urlshortner.model.UrlData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UrlDataRepo extends JpaRepository<UrlData, String> {
-
+    Optional<UrlData> findByShortUrl(String shortUrl);
 }
