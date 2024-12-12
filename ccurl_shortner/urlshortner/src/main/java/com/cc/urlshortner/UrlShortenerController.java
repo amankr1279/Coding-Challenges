35da6c267642c4c4604d380a6aa9cd40cb38b716
Aman Kumar <aman.q.kumar@oracle.com>
2024-12-12 12:51:09 +0530

    Handle exceptions in /add and create redirect url

--- ccurl_shortner/urlshortner/src/main/java/com/cc/urlshortner/UrlShortenerController.java
+++ ccurl_shortner/urlshortner/src/main/java/com/cc/urlshortner/UrlShortenerController.java
@@ -3,12 +3,10 @@ package com.cc.urlshortner;
 import com.cc.urlshortner.model.AddUrlRequest;
 import com.cc.urlshortner.model.AddUrlResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UrlShortenerController {
@@ -19,17 +17,37 @@ public class UrlShortenerController {
     @PostMapping("/add")
     public ResponseEntity<AddUrlResponse> postURL(@RequestBody AddUrlRequest body) {
         System.out.println(body.toString());
-        String shortUrl = shortenerService.shortenUrl(body.getLongUrl());
         AddUrlResponse addUrlResponse = new AddUrlResponse();
-        addUrlResponse.setShortUrl("http://localhost:8080/" + shortUrl);
-        addUrlResponse.setLongUrl(body.getLongUrl());
-        addUrlResponse.setKey(shortUrl);
-        ResponseEntity<AddUrlResponse> response = new ResponseEntity<AddUrlResponse>(addUrlResponse, HttpStatus.OK);
-        return response;
+        try {
+            String shortUrl = shortenerService.shortenUrl(body.getLongUrl());
+            addUrlResponse.setShortUrl("http://localhost:8080/" + shortUrl);
+            addUrlResponse.setLongUrl(body.getLongUrl());
+            addUrlResponse.setKey(shortUrl);
+            ResponseEntity<AddUrlResponse> response = new ResponseEntity<>(addUrlResponse, HttpStatus.OK);
+            return response;
+        } catch (Exception e) {
+            addUrlResponse.setMessage("Bad Request");
+            ResponseEntity<AddUrlResponse> response = new ResponseEntity<>(addUrlResponse, HttpStatus.BAD_REQUEST);
+            return response;
+        }
     }
 
     @DeleteMapping("/remove")
     public void removeUrl() {
 
+    }
+
+    @RequestMapping("/{key}")
+    public ResponseEntity<String> redirect(@PathVariable("key") String key) {
+        String longUrl = shortenerService.getLongUrl(key);
+        if (longUrl.isBlank()) {
+            ResponseEntity<String> response = new ResponseEntity<>("Bad Request\n", HttpStatus.BAD_REQUEST);
+
+            return response;
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("location", longUrl);
+        ResponseEntity<String> response = new ResponseEntity<>("Redirecting\n",headers, HttpStatus.FOUND);
+        return response;
     }
 }
